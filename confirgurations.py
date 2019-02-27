7e64259862b762126758c7a86898d8c4c98b0ca0
Jatin Parab <sulphurgfx@gmail.com>
2019-02-27 15:59:06 +0530

    first commit

---
+++ confirgurations.py
@@ -0,0 +1,8 @@
+config = {
+    "apiKey": "AIzaSyBoJ4It1lLhl8IKDdQy8jfLSGC-AC6P6Cs",
+    "authDomain": "raiteval.firebaseapp.com",
+    "databaseURL": "https://raiteval.firebaseio.com",
+    "projectId": "raiteval",
+    "storageBucket": "raiteval.appspot.com",
+    "messagingSenderId": "251247045473"
+}
